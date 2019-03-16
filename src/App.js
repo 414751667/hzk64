dbfe1b23397c25ec0f8b89434c05f2431e92bfd8
414751667 <414751667@qq.com>
2019-03-16 10:52:49 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import {Button} from 'antd-mobile';
+import 'antd-mobile/dist/antd-mobile.css';  
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       App----
+       <Button>按钮</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
